d0315db6377d5f7fb8b3ba8283f0fccad4d25406
Peter Saville <root@x360.lan>
2020-04-15 21:07:16 +1000

    Adding ShootyMcShooter

---
+++ ShootyMcShooter/Assets/__Scripts/Hero.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    static public Hero S;       // Singleton Pattern
+
+    [Header("Set In Inspector")]
+    // These fields control the movement of the ship
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    [Header("Set Dynamically")]
+    public float shieldLevel = 1;
+    
+    void Awake () {
+        if (S == null) {
+            S=this;             // Set the singleton
+        } else {
+            Debug.LogError("Hero.Awake() - Attempted to assign a second Hero.S!");
+        }
+    
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Pull in info from the Input Class
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        // Change the transform.position based on the axes
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Rotate the ship to make it feel more dynamic
+        transform.rotation = Quaternion.Euler(yAxis*pitchMult, xAxis * rollMult, 0);
+        
+    }
+}
---
+++ ShootyMcShooter/Assets/__Scripts/Shield.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    [Header("Set In Inspector")]
+    public float rotationsPerSecond = 0.1f;
+
+    [Header("Set Dynamically")]
+    public int levelShown = 0;
+
+    Material mat;       // Non-public variable will not appear in the Inspector
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
